486259e576aca7d63e663d0d76dca60ffe791374
mrcmrcmrc <miraccaylak@gmail.com>
2017-01-17 16:52:21 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace wallpapers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OK = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OK
+            // 
+            this.OK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OK.Location = new System.Drawing.Point(691, 13);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(75, 47);
+            this.OK.TabIndex = 1;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton1.Location = new System.Drawing.Point(58, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(41, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.Text = "day";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton2.Location = new System.Drawing.Point(106, 21);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(50, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.Text = "week";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton3.Location = new System.Drawing.Point(163, 21);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(53, 17);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.Text = "month";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton4.Location = new System.Drawing.Point(223, 21);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(44, 17);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.Text = "year";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton5.Location = new System.Drawing.Point(274, 21);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(56, 17);
+            this.radioButton5.TabIndex = 6;
+            this.radioButton5.Text = "all time";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.numericUpDown1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.numericUpDown1.Location = new System.Drawing.Point(561, 40);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Checked = true;
+            this.radioButton6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton6.Location = new System.Drawing.Point(6, 21);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(45, 17);
+            this.radioButton6.TabIndex = 8;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "hour";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton7.Location = new System.Drawing.Point(61, 21);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(39, 17);
+            this.radioButton7.TabIndex = 10;
+            this.radioButton7.Text = "top";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            this.radioButton7.CheckedChanged += new System.EventHandler(this.radioButton7_CheckedChanged);
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Checked = true;
+            this.radioButton8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton8.Location = new System.Drawing.Point(15, 21);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(39, 17);
+            this.radioButton8.TabIndex = 11;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "hot";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton9);
+            this.groupBox1.Controls.Add(this.radioButton8);
+            this.groupBox1.Controls.Add(this.radioButton7);
+            this.groupBox1.Location = new System.Drawing.Point(20, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(167, 54);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "short";
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton9.Location = new System.Drawing.Point(107, 21);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(44, 17);
+            this.radioButton9.TabIndex = 12;
+            this.radioButton9.Text = "new";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.OK);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Location = new System.Drawing.Point(12, 529);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1066, 70);
+            this.panel1.TabIndex = 15;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "wallpapers",
+            "animeWallpaper",
+            "Wallpaper",
+            "MinimalWallpaper",
+            "EarthPorn",
+            "sfwpornnetwork",
+            "MoviePosterPorn",
+            "CityPorn",
+            "FoodPorn"});
+            this.comboBox1.Location = new System.Drawing.Point(561, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 14;
+            this.comboBox1.Text = "subreddit...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(854, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "label1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton6);
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Enabled = false;
+            this.groupBox2.Location = new System.Drawing.Point(202, 7);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(343, 54);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "time";
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1066, 505);
+            this.panel2.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
+            this.ClientSize = new System.Drawing.Size(1090, 611);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using HtmlAgilityPack;
+
+namespace wallpapers
+{
+    public partial class Form1 : Form
+    {
+        List<string> images;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            images = new List<string>();
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clear();
+            Execute();
+        }
+
+        private void Execute()
+        {
+            string lastPostID = "";
+
+            /* Uri url = new Uri("http://www.google.com");
+           WebClient client = new WebClient();
+           string html = client.DownloadString(url);
+           //File.WriteAllText("mytext.txt", html);// 
+           */
+
+            HtmlWeb hw = new HtmlWeb();
+
+            HtmlAgilityPack.HtmlDocument doc = null;
+            Console.WriteLine(hw.StatusCode);
+            for (int p = 0; p < numericUpDown1.Value; p++)
+            {
+                if (p != 0)
+                    lastPostID = getLastPostID(doc);
+                string myURL = createQueryString(p, lastPostID);
+                Console.WriteLine(myURL);
+                doc = hw.Load(myURL);
+                images.AddRange(getImageLinks(doc));
+
+                //doc = hw.Load(myURL);
+                // // images.AddRange(GetImgurLinks(ExtractAllAHrefTags(doc)));
+                //images.AddRange(getImageLinks(doc));
+            }
+
+            createUI();
+        }
+
+
+        private void Clear()
+        {
+            images.Clear();
+           
+            foreach (PictureBox pb in panel2.Controls)
+            {
+                Console.WriteLine("pb");
+                pb.Dispose();
+            }
+          //  panel2.Dispose();
+            panel2.Controls.Clear();
+        }
+
+        private string createQueryString(int p, string lastID)
+        {
+            string baseuri = "https://www.reddit.com/r/";
+            string t = "";
+            string nextPage = "?count=" + 25 * p + "&after=" + lastID;
+            string subreddit = comboBox1.Text;
+
+            if (comboBox1.Text == "sfwpornnetwork")
+            {
+                baseuri = "https://www.reddit.com/user/";
+                subreddit = "kjoneslol/m/sfwpornnetwork";
+            }
+            if (radioButton7.Checked)
+            {
+                if (radioButton1.Checked)
+                    t = "/top/?sort=top&t=day";
+                else if (radioButton2.Checked)
+                    t = "/top/?sort=top&t=week";
+                else if (radioButton3.Checked)
+                    t = "/top/?sort=top&t=month";
+                else if (radioButton4.Checked)
+                    t = "/top/?sort=top&t=year";
+                else if (radioButton5.Checked)
+                    t = "/top/?sort=top&t=all";
+                else if (radioButton6.Checked)
+                    t = "/top/?sort=top&t=hour";
+
+                if (p == 0)
+                    return  baseuri + subreddit + t;
+                else
+                {
+                    nextPage = "&count=" + 25 * p + "&after=" + lastID; //zamana göre sıralama yapılabilenlerde ?count yerine &count
+                    return baseuri + subreddit + t + nextPage;
+                }
+                    
+                }
+
+            else if(radioButton8.Checked)
+            {
+                if (p == 0)
+                    return baseuri + subreddit + "/hot";
+                else
+                    return baseuri + subreddit + "/hot" + nextPage;
+            }
+
+            else if(radioButton9.Checked)
+            {
+                if (p == 0)
+                    return baseuri + subreddit + "/new";
+                else
+                    return baseuri + subreddit + "/new" + nextPage;
+            }
+
+            return null;
+              
+        }
+
+        private string getLastPostID(HtmlAgilityPack.HtmlDocument _doc) 
+        {
+            var allElementsWithClassFloat = _doc.DocumentNode.SelectNodes("//*[contains(@class,'thing')]");
+
+            return "t3_" + (allElementsWithClassFloat[allElementsWithClassFloat.Count-1].Id.Split('_'))[2];
+
+        } // birden fazla sayfaya bakarken bir sonraki sayfaya geçebilmek için son post'un id'sine ihtiyaç var.
+
+        private List<string> ExtractAllAHrefTags(HtmlAgilityPack.HtmlDocument htmlSnippet)
+        {
+            List<string> hrefTags = new List<string>();
+
+            foreach (HtmlNode link in htmlSnippet.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                HtmlAttribute att = link.Attributes["href"];
+                hrefTags.Add(att.Value);
+            }
+
+            return hrefTags;
+        }// ilk kullandığım yöntem. bütün href leri alıyor.
+
+        private List<string> GetImgurLinks(List<string> links)
+        {
+            List<string> l = new List<string>();
+            for (int i = 0; i < links.Count; i++)
+            {
+                if (links[i].Contains("domain"))
+                    continue;
+                if (links[i].Contains("reddituploads"))
+                {
+                    links[i] = convertURLreddit(links[i]);
+                    l.Add(links[i]);
+                    i++;
+                    continue;
+                }
+                if (links[i].Contains("imgur"))
+                {
+                    if (!(links[i].Contains(".jpg") || links[i].Contains(".png")))
+                        links[i] = convertURL(links[i]);
+                    l.Add(links[i]);
+                    //Console.WriteLine(links[i]);
+                    i++;               //i++'lar 2 defa eklemesin diye.
+                }
+
+            }
+            return l;
+        }//bütün hreflerin içinden imgur linklerini almak için. eski yöntem
+
+        private string convertURL(string s)
+        {
+            string[] st = new string[10];
+            foreach (string x in s.Split('/'))
+                st[2] = x;
+
+            return s = "https://i.imgur.com/" + st[2] + ".jpg";
+        }//imgur imageleri için.eski yöntem.
+
+        private List<string> getImageLinks(HtmlAgilityPack.HtmlDocument htmldoc)
+        {
+            List<string> links = new List<string>();
+
+            foreach (HtmlNode divnode in htmldoc.DocumentNode.SelectNodes("//div[@data-url]"))
+            {
+                HtmlAttribute att = divnode.Attributes["data-url"];
+                if (att.Value.Contains("comments"))
+                    continue;
+                if (att.Value.Contains("reddituploads"))
+                {
+                    links.Add(convertURLreddit(att.Value));
+                    continue;
+                }
+                if (!att.Value.Contains(".jpg") && !att.Value.Contains(".png"))
+                    continue;
+                links.Add(att.Value);
+               
+            }
+
+            return links;
+        } // yeni yöntem. data-url attribute'nden image linklerini al
+
+        private string convertURLreddit(string s) 
+        {
+            string[] temp = s.Split('?');
+            string[] str = temp[1].Split(new string[] { "amp;" }, StringSplitOptions.None);
+            string n = "";
+            foreach (string i in str)
+                n += i;
+            return temp[0] + "?" + n;
+        } // reddituploads.com'a yüklenen imageleri almak için.
+
+        private void createUI()
+        {
+
+            int x = 0;
+            int y = 0;
+            int marginY = 10;
+            int marginX = 10;
+            PictureBox[] pb = new PictureBox[images.Count];
+
+            for (int i = 0; i < images.Count; i++)
+            {
+               
+                pb[i] = new PictureBox();
+                pb[i].Location = new Point(5 + x, y);
+                pb[i].Size = new Size(250, 210);
+                pb[i].SizeMode = PictureBoxSizeMode.StretchImage;
+            
+                pb[i].ImageLocation = images[i];
+                pb[i].Name = "pb" + i.ToString();
+                pb[i].BorderStyle = BorderStyle.FixedSingle;
+                pb[i].MouseClick += SaveAs_Click;
+                //this.Controls.Add(pb[i]);
+                panel2.Controls.Add(pb[i]);
+              
+                x += pb[i].Size.Width + marginX;
+                if(x+pb[i].Size.Width >= panel2.Size.Width)
+                {
+                    x = 0;
+                    y += pb[i].Size.Height + marginY;
+                }
+                label1.Text = i.ToString();        
+            }
+
+            label1.Text = images.Count + "....";
+        }
+
+        void SaveAs_Click(object sender, MouseEventArgs e)
+        {
+            PictureBox pbox = sender as PictureBox;
+
+            // handle click event
+            if (e.Button == MouseButtons.Left)
+            {
+                pbox.Image.Save(pbox.Name + ".jpg");
+            }
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton7.Checked)
+            {
+                groupBox2.Enabled = true;
+            }
+            else
+                groupBox2.Enabled = false;
+        }
+    }
+
+}
